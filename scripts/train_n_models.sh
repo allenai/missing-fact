1ababc349344f8f00861c13294bffb328b05cbec
Michael Schmitz <michaels@allenai.org>
2019-09-17 07:59:22 -0700

    Reset cuda device to 0

--- scripts/train_n_models.sh
+++ scripts/train_n_models.sh
@@ -5,7 +5,7 @@ set -e
 t=${NUM-5}
 outdir=${OUTDIR-/output}
 config=${CONF-/config.json}
-cuda_device=-1
+cuda_device==${CUDA-0}
 file_list=""
 for n in `seq 1 $t`; do
     run_dir=${outdir}/run${n}
